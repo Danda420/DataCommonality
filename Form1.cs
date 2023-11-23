2cda2415cb0147eed4ad15cda408f419770bb2fc
Danda420 <dandagaharu77@gmail.com>
2023-11-23 20:01:59 +0700

    read files in a folder instead of one file

--- Form1.cs
+++ Form1.cs
@@ -12,12 +12,14 @@ using System.Windows.Forms;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace DataCommonalityChecker
 {
     public partial class Form1 : Form
     {
-        public string sourceCode;
+        private List<string> sourceCodeFile = new List<string>();
+        public string fileName;
 
         public void updateVarCount(Dictionary<string, int> varCount, string varName)
         {
@@ -38,14 +40,23 @@ namespace DataCommonalityChecker
 
         private void btnBrowse_Click(object sender, EventArgs e)
         {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Title = "Open Source Code File";
-            dialog.Filter = "C# Source Code Files|*.cs";
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.Title = "Open Source Code Folder";
+            dialog.IsFolderPicker = true;
 
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
-                sourceCode = File.ReadAllText(dialog.FileName);
+                sourceCodeFile.Clear();
 
+                string folderPath = dialog.FileName;
+                string[] csFiles = Directory.GetFiles(folderPath, "*.cs");
+
+                foreach (var file in csFiles)
+                {
+                    string fileName = Path.GetFileName(file);
+                    string fileContent = File.ReadAllText(file);
+                    sourceCodeFile.Add(fileContent);
+                }
             }
         }
 
@@ -53,69 +64,80 @@ namespace DataCommonalityChecker
         {
             dataGridView1.Rows.Clear();
             label2.Text = string.Empty;
-            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
-            var root = syntaxTree.GetRoot();
             Dictionary<string, int> varCount = new Dictionary<string, int>();
-            var fieldDec = root.DescendantNodes().OfType<FieldDeclarationSyntax>();
-            var propertyDec = root.DescendantNodes().OfType<PropertyDeclarationSyntax>();
-            var classDec = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
-            int totalModules = root.DescendantNodes().OfType<ClassDeclarationSyntax>().Count() + root.DescendantNodes().OfType<MethodDeclarationSyntax>().Count();
+            int totalModules = 0;
 
-            if (sourceCode == null)
+            foreach (var sourceCode in sourceCodeFile)
             {
-                label2.Text = "No File Supplied!";
-            }
-            else
-            {
-                foreach (var fDec in fieldDec)
-                {
-                    var varDec = fDec.DescendantNodes().OfType<VariableDeclaratorSyntax>();
-                    foreach (var vDec in varDec)
-                    {
-                        string varName = vDec.Identifier.Text;
+                SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+                var root = syntaxTree.GetRoot();
 
-                        if (!varName.StartsWith("_") && char.IsLower(varName[0]) && (varName != "var"))
-                        {
-                            updateVarCount(varCount, varName);
-                        }
-                    }
+                totalModules += root.DescendantNodes().OfType<ClassDeclarationSyntax>().Count() +
+                                root.DescendantNodes().OfType<MethodDeclarationSyntax>().Count();
+
+                dataGridView1.Rows.Clear();
+                label2.Text = string.Empty;
+                var fieldDec = root.DescendantNodes().OfType<FieldDeclarationSyntax>();
+                var propertyDec = root.DescendantNodes().OfType<PropertyDeclarationSyntax>();
+                var classDec = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+
+                if (sourceCode == null)
+                {
+                    label2.Text = "No File Supplied!";
                 }
-
-                foreach (var pDec in propertyDec)
+                else
                 {
-                    string varName = pDec.Identifier.Text;
-                    updateVarCount(varCount, varName);
-                }
-
-                foreach (var cDec in classDec)
-                {
-                    var methodDec = cDec.DescendantNodes().OfType<MethodDeclarationSyntax>();
-                    foreach (var mDec in methodDec)
+                    foreach (var fDec in fieldDec)
                     {
-                        var addedVars = new HashSet<string>();
-
-                        var varUsage = mDec.DescendantNodes().OfType<IdentifierNameSyntax>();
-                        foreach (var usage in varUsage)
+                        var varDec = fDec.DescendantNodes().OfType<VariableDeclaratorSyntax>();
+                        foreach (var vDec in varDec)
                         {
-                            string varName = usage.Identifier.Text;
+                            string varName = vDec.Identifier.Text;
 
                             if (!varName.StartsWith("_") && char.IsLower(varName[0]) && (varName != "var"))
                             {
-                                if (addedVars.Add(varName))
+                                updateVarCount(varCount, varName);
+                            }
+                        }
+                    }
+
+                    foreach (var pDec in propertyDec)
+                    {
+                        string varName = pDec.Identifier.Text;
+                        updateVarCount(varCount, varName);
+                    }
+
+                    foreach (var cDec in classDec)
+                    {
+                        var methodDec = cDec.DescendantNodes().OfType<MethodDeclarationSyntax>();
+                        foreach (var mDec in methodDec)
+                        {
+                            var addedVars = new HashSet<string>();
+
+                            var varUsage = mDec.DescendantNodes().OfType<IdentifierNameSyntax>();
+                            foreach (var usage in varUsage)
+                            {
+                                string varName = usage.Identifier.Text;
+
+                                if (!varName.StartsWith("_") && char.IsLower(varName[0]) && (varName != "var"))
                                 {
-                                    updateVarCount(varCount, varName);
+                                    if (addedVars.Add(varName))
+                                    {
+                                        updateVarCount(varCount, varName);
+                                    }
                                 }
                             }
                         }
                     }
+
+                    label2.Text = "Total Number of Modules : " + totalModules.ToString();
+                    foreach (var a in varCount)
+                    {
+                        float dataCommonality = ((float)a.Value / totalModules) * 100;
+                        dataGridView1.Rows.Add(a.Key, a.Value, $"{dataCommonality}%");
+                    }
                 }
 
-                label2.Text = "Total Number of Modules : " + totalModules.ToString();
-                foreach (var a in varCount)
-                {
-                    float dataCommonality = ((float)a.Value / totalModules) * 100;
-                    dataGridView1.Rows.Add(a.Key, a.Value, $"{dataCommonality}%");
-                }
             }
         }
 
